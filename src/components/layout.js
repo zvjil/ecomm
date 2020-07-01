9e36aa431135fb0361c8618f338c33d68d6535da
Jill <zvjil@hotmail.com>
2020-07-01 13:50:01 -0400

    built header and navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import Header from './headernavbar/header';
+import Navbar from './headernavbar/navbar';
 
 class Layout extends Component {
   render () {
     return (
-      <div className="app">
+      <div className="layout">
         {this.props.children}
+        <Header />
+        <Navbar />
       </div>
     );
   }

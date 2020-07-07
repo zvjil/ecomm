682c8b73487652560fed03d2e4356ead644e6637
Jill <zvjil@hotmail.com>
2020-07-07 12:12:36 -0400

    built details component for quick links

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -1,12 +1,34 @@
 import React, {Component} from 'react';
+
 import {reduxForm, Field} from 'redux-form';
+
 import {FormInput, FormButton} from '../formFields';
+import Details from '../details';
+
+import history from '../../history';
 
 class SignInForm extends Component {
   render () {
     const {className, handleSubmit} = this.props;
+    const links = [
+      {
+        _id: 0,
+        title: 'Not registered? Create account here',
+        onClick: () => history.push ('/signup'),
+      },
+      {
+        _id: 1,
+        title: 'Forgot account email?',
+        onClick: () => console.log ('forgot email'),
+      },
+      {
+        _id: 2,
+        title: 'Forgot password?',
+        onClick: () => console.log ('forgot password'),
+      },
+    ];
     return (
-      <form onSubmit={handleSubmit} className={`${className}sign-in-form`}>
+      <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
         <Field
           className="sign-in-form__email"
           type="email"
@@ -15,7 +37,6 @@ class SignInForm extends Component {
           name="email"
           component={FormInput}
         />
-
         <Field
           className="sign-in-form__password"
           type="password"
@@ -27,11 +48,15 @@ class SignInForm extends Component {
         <div className="sign-in-form__line" />
         <Field
           className="sign-in-form__login"
-          onClick={() => console.log ('trying to submit')}
           type="submit"
           title="Login"
           name="login"
           component={FormButton}
+        />
+        <Details
+          className="sign-in-form__details"
+          title="QuickLinks"
+          links={links}
         />
       </form>
     );

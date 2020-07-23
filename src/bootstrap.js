b8e59456a6cfbc43273eb3bf9b78b85e899c1645
Jill <zvjil@hotmail.com>
2020-07-23 14:12:55 -0400

    built out shipping and payment components

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -9,6 +9,9 @@ import SignUp from './components/auth/signup';
 import reducers from './reducers';
 import Account from './components/account/account';
 import Shop from './components/shop/shop';
+import Review from './components/order/review';
+import Shipping from './components/information/shipping';
+import Payment from './components/information/payment';
 
 const createStoreWithMiddleware = applyMiddleware () (
   compose (
@@ -34,6 +37,9 @@ function main () {
             <Route path="/account" exact component={Account} />
 
             <Route path="/shop" exact component={Shop} />
+            <Route path="/order/review" exact component={Review} />
+            <Route path="/information/shipping" exact component={Shipping} />
+            <Route path="/information/payment" exact component={Payment} />
           </Switch>
         </Layout>
       </Router>
